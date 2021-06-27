23ce8881d1e8b68ee8038d196589ea18a99813c2
Joao Gabriel <joao.gabrielfjob@gmail.com>
2021-06-27 20:55:00 -0300

    :heavy_plus_sign: app + mongo + env dependencies

---
+++ src/config/deps.ts
@@ -0,0 +1,5 @@
+export { MongoClient } from 'https://deno.land/x/mongo@v0.23.1/mod.ts'
+
+export { Application, Router, Context } from 'https://deno.land/x/oak@v7.6.3/mod.ts'
+
+export { config } from 'https://deno.land/x/dotenv@v2.0.0/mod.ts'
